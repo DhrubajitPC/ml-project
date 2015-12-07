d8d9266c3fa60fdb1f3e2cdbc43011b40ba83b10
Dhrubajit <dhrubajit1992@gmail.com>
2015-12-08 02:39:51 +0800

    hopefully final ver part4

--- read_test.py
+++ read_test.py
@@ -11,7 +11,7 @@ def insert_val(x, li):
 			li[i] = x
 			inserted = True
 			break
-	return inserted,i
+	return inserted, i
 # li =[]
 # for i in range(1,10):
 # 	li.append(np.random.randint(10))
@@ -22,12 +22,5 @@ li2 = [4, 2, 8, 5, 8, 6, 7, 0, 0]
 li2.sort(reverse = True)
 # print li2
 
-# x,y =  insert_val(0,li2)
-# print x, y, li2
-
-
-a = {1: 23, 2: 45}
-if 1 in a:
-	print "x"
-else:
-	print "y"
+x,y =  insert_val(7,li2)
+print x, y, li2
